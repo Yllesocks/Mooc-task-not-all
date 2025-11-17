6dc9353393d0e00dd323a76989d765d8ec90c6d6
Yllesocks <yllesocks@gmail.com>
2025-11-17 14:26:21 +0200

    -Added the code for part3-4-T6-T7

--- python/Part-3/Part3-4/part3-4-T7.py
+++ python/Part-3/Part3-4/part3-4-T7.py
@@ -1,9 +1,13 @@
 def squared(word,x):
     index = 0
-    while index < x:
-        word = "1010101010101010" * x
-        print(word[index: index + x])
+    row = ""
+    while index < x * x:
+        if index > 0 and index % x == 0:
+            print(row)
+            row = ""
+        row += word[index % len(word)]
         index += 1
+    print(row)
 
 if __name__ == "__main__":
-    squared("abc",5)
+    squared("ab", 3)
