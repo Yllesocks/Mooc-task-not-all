9b0957348b6037de66e7be5ce63e08bac049297a
Lukas <lukas.ekroos@eduespoo.fi>
2025-11-17 11:00:38 +0200

    Added part3-4-T1-T5

---
+++ python/Part3-4/part3-4-T4.py
@@ -0,0 +1,8 @@
+def print_many_times(text, times):
+    index = 0
+    while index < times:
+        print(text)
+        index += 1
+
+if __name__ == "__main__":
+    print_many_times("python", 5)
---
+++ python/Part3-4/part3-4-T5.py
@@ -0,0 +1,8 @@
+def hash_square(times):
+    index = 0
+    while index < times:
+        print("#" * times)
+        index += 1
+
+if __name__ == "__main__":
+    hash_square(5)
