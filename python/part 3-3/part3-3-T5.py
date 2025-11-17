ab7dba41c214f516d21265fb9e520c44f6a52ad1
Lukas <lukas.ekroos@eduespoo.fi>
2025-11-17 10:27:57 +0200

    -Added part3-3-T5

--- python/part 3-3/part3-3-T5.py
+++ python/part 3-3/part3-3-T5.py
@@ -1,16 +1,12 @@
 num = int(input("Please type in a number:"))
 num1 = 1
 num2 = num
-while num % 2 == 0 and num1 <= num:
-    print(num1)
-    print(num2)
-    num1 += 1
-    num2 -= 1
-while num % 2 != 0:
-    if num1 > num:
-        print(num)
+while num1 <= num2:
+    if num1 != num2:
+        print(num1)
+        print(num2)
+        num1 += 1
+        num2 -= 1
+    else:
+        print(num1)
         break
-    print(num1)
-    print(num2)
-    num1 += 2
-    num2 += 2
