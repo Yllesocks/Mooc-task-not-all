00311a3eccd37a60c562a77e15b71b74f6c825e5
Yllesocks <lukas.ekroos5@gmail.com>
2026-02-03 14:21:06 +0200

    chat doesnt work.....
    help me

--- python/Part-6/part6-1/part6-1-T6.py
+++ python/Part-6/part6-1/part6-1-T6.py
@@ -1,52 +1,50 @@
 student_info = input("Student information: ")
 exercise_data = input("Exercises completed: ")
-exam_data = input("Exam points: ")
+exam_points = input("Exam points: ")
 
-students = {}
-with open(student_info) as file:
-    next(file) 
-    for line in file:
-        id_, first, last = line.strip().split(";")
-        students[id_] = f"{first} {last}"
 
-exercises_done = {}
-exercise_points = {}
-with open(exercise_data) as file:
-    next(file) 
-    for line in file:
-        parts = line.strip().split(";")
-        id_ = parts[0]
-        total = sum(int(x) for x in parts[1:])
-        exercises_done[id_] = total
-        exercise_points[id_] = total // 4
 
-exam_points = {}
-with open(exam_data) as file:
-    next(file) 
-    for line in file:
-        parts = line.strip().split(";")
-        id_ = parts[0]
-        exam_points[id_] = sum(int(x) for x in parts[1:])
+def create_dict(file):
+	new_dict = {}
+	with open(file) as new_file:
+		for line in new_file:
+			line = line.strip()
+			parts = line.split(";")
+			if parts[0] == "id":
+				continue
+			new_dict[parts[0]] = []
+			for item in parts[1:]:
+				if item.isdigit():
+					new_dict[parts[0]].append(int(item))
+				else:
+					new_dict[parts[0]].append(item)
+	return new_dict
 
-print()
-print(f"{'name':30}{'exec_nbr':>10}{'exec_pts.':>10}{'exm_pts.':>10}{'tot_pts.':>10}{'grade':>10}")
+def comb_dict(student_names, student_exercises, student_exams):
+	print(f"{'name':30}{'exec_nbr':10}{'exec_pts.':10}{'exm_pts.':10}{'tot_pts.':10}{'grade':10}")
+	for id, name in student_names.items():
+		if id in student_exercises:
+			grade = calculate_grade(sum(student_exercises[id]), sum(student_exams[id]))
+			full_name = f"{name[0]} {name[1]}"
+			print(f"{full_name:30}{grade[0]:<10}{grade[1]:<10}{grade[2]:<10}{grade[3]:<10}{grade[4]:<10}")
 
-for id_ in students:
-    exec_nbr = exercises_done[id_]
-    exec_pts = exercise_points[id_]
-    exm_pts = exam_points[id_]
-    tot_pts = exec_pts + exm_pts
+def calculate_grade(exercises, exam_points):
+	point_exerc = int((exercises/40) * 10)
+	total_points = point_exerc + exam_points
+	grade = 5
+	if total_points <= 14:
+		grade = 0
+	elif total_points <= 17:
+		grade = 1
+	elif total_points <= 20:
+		grade = 2
+	elif total_points <= 23:
+		grade = 3
+	elif total_points <= 27:
+		grade = 4
+	return [exercises, point_exerc, exam_points, total_points, grade]	
 
-    if tot_pts <= 14:
-        grade = 0
-    elif tot_pts <= 17:
-        grade = 1
-    elif tot_pts <= 20:
-        grade = 2
-    elif tot_pts <= 23:
-        grade = 3
-    elif tot_pts <= 27:
-        grade = 4
-    else:
-        grade = 5
-    print(f"{students[id_]:30}{exec_nbr:>10}{exec_pts:>10}{exm_pts:>10}{tot_pts:>10}{grade:>10}")
+student_names = create_dict(student_info)
+student_exercises = create_dict(exercise_data)
+student_exams = create_dict(exam_points)
+comb_dict(student_names, student_exercises, student_exams)
